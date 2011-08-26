ec694ca1ee08ef7d83067bc6ac5d116ced18cde5
Trotter Cashion <cashion@gmail.com>
2011-08-25 20:27:21 -0700

    Receive a call

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'rubygems'
 require 'sinatra'
+require 'builder'
 
 get '/' do
   '<html><head><title>SV.rb Phone Service</title></head><body><h1>Call (650) 318-6254</h1></body></html>'
+end
+
+post '/when-is-meeting' do
+  xml = Builder::XmlMarkup.new
+  xml.Response do |resp|
+    resp.Say "Jose Valim Speaks about Rails 3.1"
+  end
 end
