9c20894c38d9cfdfb77c9931474e2b412614422c
Trotter Cashion <cashion@gmail.com>
2011-08-25 21:06:37 -0700

    Ask meetup where the meeting is

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 require 'builder'
+require 'nokogiri'
+require 'open-uri'
+
+RSS_FEED = "http://www.meetup.com/silicon-valley-ruby/events/rss/Silicon+Valley+Ruby+Meetup/"
 
 get '/' do
   '<html><head><title>SV.rb Phone Service</title></head><body><h1>Call (650) 318-6254</h1></body></html>'
@@ -9,6 +13,16 @@ end
 post '/when-is-meeting' do
   xml = Builder::XmlMarkup.new
   xml.Response do |resp|
-    resp.Say "Jose Valim Speaks about Rails 3.1 on Thursday September 15th at 7pm at Carnegie Mellon, Building 23 at NASA"
+    rss = Nokogiri::XML(open(RSS_FEED))
+    event_url = rss.xpath('//guid').first.text
+    html = Nokogiri::HTML(open(event_url))
+
+    about_event = html.xpath('//h1[@itemprop="summary"]').text
+    about_event << " on "
+    about_event << html.xpath('//p[@class="headline"]').text.gsub("\n", " ")
+    about_event << " at "
+    about_event << html.xpath('//div[@id="event-where-display"]/span[@itemprop="locality"]').text.gsub("\n", " ")
+
+    resp.Say about_event
   end
 end
